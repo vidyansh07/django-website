79562c670351cf4ec3fb1024cf84f0ac0c082b35
Vidyansh07 <Vidyansht@gmail.com>
2024-01-01 23:39:45 +0530

    It's my portfolio

---
+++ script.js
@@ -0,0 +1,48 @@
+function firstImage(){
+    let x = document.getElementById("image");
+    x.src = "images/e2.jpeg";
+}
+function secondImage(){
+    let x = document.getElementById("image");
+    x.src = "images/e3.jpeg";
+}
+function thirdImage(){
+    let x = document.getElementById("image");
+    x.src = "images/e4.jpeg";
+}
+function fouthImage(){
+    let x = document.getElementById("image");
+    x.src = "images/e1.jpeg";
+}
+function e(){
+    let x = document.getElementById("image");
+    x.src = "images/e5.jpeg";
+}
+function e1(){
+    let x = document.getElementById("image");
+    x.src = "images/e6.jpeg";
+}
+function e2(){
+    let x = document.getElementById("image");
+    x.src = "images/e7.jpeg";
+}
+function e3(){
+    let x = document.getElementById("image");
+    x.src = "images/e8.jpeg";
+}
+function e4(){
+    let x = document.getElementById("image");
+    x.src = "images/e9.jpeg";
+}
+function e5(){
+    let x = document.getElementById("image");
+    x.src = "images/e15.jpeg";
+}
+function e6(){
+    let x = document.getElementById("image");
+    x.src = "images/e14.jpeg";
+}
+function e7(){
+    let x = document.getElementById("image");
+    x.src = "images/e11.jpeg";
+}
